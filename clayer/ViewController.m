b242cefac4da7cdff7c3a2ec3df5cecb039bdb89
HU <hmenghu@qq.com>
2016-06-27 20:16:33 +0800

    dsfj；lafj
    进；ajfa；

--- clayer/ViewController.m
+++ clayer/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)jieping:(id)sender;
 
 @property (weak, nonatomic) IBOutlet UIImageView *mypic;
 
@@ -20,8 +21,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+ 
+}
+-(void)testmy
+{
     CGSize size = CGSizeMake(200, 200);
-   
+    
     
     UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
     
@@ -31,17 +36,15 @@
     CGContextAddArc(ctr, 100, 100, 100, 0, M_PI*2, 0);
     CGContextFillPath(ctr);
     
-  //  [[UIColor redColor] set];
     CGContextAddArc(ctr, 100, 100, 96, 0, M_PI*2, 0);
-  //  CGContextFillPath(ctr);
     
     CGContextClip(ctr);
     
     UIImage *oldimage = [UIImage imageNamed:@"mypic"];
+    
     CGRect rect = CGRectMake(2, 2, 196, 196);
+    
     [oldimage drawInRect:rect];
-    
-    
     
     UIImage *newimage = UIGraphicsGetImageFromCurrentImageContext();
     
@@ -55,7 +58,6 @@
     
     self.mypic.image = newimage;
 }
-
 -(void)test
 {
     UIImage *oldimage = [UIImage imageNamed:@"mypic"];
@@ -88,4 +90,23 @@
 
 
 
+- (IBAction)jieping:(id)sender {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+    
+    UIGraphicsBeginImageContextWithOptions(self.view.frame.size, NO, 0.0);
+    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    
+    UIImage *newimage = UIGraphicsGetImageFromCurrentImageContext();
+    NSData *data = UIImagePNGRepresentation(newimage);
+    UIGraphicsEndImageContext();
+    
+    NSString *path = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"jietu.jpg"];
+    NSLog(@"%@",path);
+    
+    [data writeToFile:path atomically:YES];
+    
+    });
+    
+}
 @end
